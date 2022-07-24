011834c96f6f4f0312c96425fa0368f786982622
Dimitra Ntzioni <dntzioni@gmail.com>
2022-07-24 23:31:35 +0100

    implement photo-viewer and add thread workers

--- api/src/router.js
+++ api/src/router.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const multer = require('multer');
+const path = require('path');
 
 const router = Router();
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
 const filename = (request, file, callback) => {
     callback(null, file.originalname)
@@ -31,6 +33,8 @@ router.post('/upload', upload.single('photo'), (request, response) => {
     if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
   
     return response.status(201).json({success: true});
-  });
+});
+
+router.get('/photo-viewer', (request, response) => {response.sendFile(photoPath)})
 
 module.exports = router;
